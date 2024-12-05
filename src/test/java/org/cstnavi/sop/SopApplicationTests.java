c7608ee10e0fdd6759c84321163d9d7e2f0fc766
Vinnnm <thanthtoo1285@gmail.com>
2024-12-05 22:39:58 +0630

    Project set up

---
+++ src/test/java/org/cstnavi/sop/SopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cstnavi.sop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
